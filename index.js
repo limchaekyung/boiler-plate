5c05ec65591a1c2ba5f8c55b4616d7b321214756
chae <chae@chae.com>
2021-10-19 02:49:15 +0900

    Auth 기능

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 3000
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
+
+const {auth} = require("./middleware/auth")
 const {User} = require("./models/User");
 
 //application/x-www-form-urlencoded
@@ -22,7 +24,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
   })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
     //회원가입 할 때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터베이스에 넣어준다.
@@ -37,7 +39,7 @@ app.post('/register', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
 
     //요청된 이메일을 데이터베이스에서 있는지 찾는다.
     User.findOne({email:req.body.email}, (err, user)=>{
@@ -69,6 +71,23 @@ app.post('/login', (req, res) => {
     })
 })
 
+app.post('/api/users/auth', auth, (req, res)=>{
+
+        //여기까지 미들웨어를 통과해 왔다는 얘기는
+        //Authentivation이 true라는 말
+        res.status(200).json({
+            _id: req.user._id,
+            //role이 0이면 일반유저, 
+            //role이 0이 아니면 관리자
+            isAdmin: req.user.role === 0 ? false : true,
+            isAuth: true,
+            email: req.user.email,
+            name: req.user.name,
+            lastname: req.user.lastname,
+            role: req.user.role,
+            image: req.user.image
+        })
+})
 app.listen(port, () => {
   console.log(`Example app listening at ${port}`)
 })
