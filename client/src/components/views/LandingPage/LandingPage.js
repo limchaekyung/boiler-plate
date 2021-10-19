c5c9a75f12781e2c5a1d74602d470b6fde3415f0
chae <chae@chae.com>
2021-10-19 21:22:15 +0900

    proxy

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import axios from 'axios'
 
 function LandingPage(){
+    
+    useEffect(() => {
+        axios.get('/api/hello')
+        .then(response => console.log(response.data))
+    }, [])
+
     return (
         <div>
-            LandingPage
+            안녕하세요!
         </div>
     )
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000
+
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
@@ -23,6 +23,10 @@ const connect = mongoose.connect(config.mongoURI)
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
+
+app.get('/api/hello', (req, res)=>{
+    res.send("안녕하세요!!!")
+})
 
 app.post('/api/users/register', (req, res) => {
 
@@ -102,6 +106,7 @@ app.get('/api/users/logout', auth, (req, res)=>{
 
 })
 
+const port = 5000
 app.listen(port, () => {
   console.log(`Example app listening at ${port}`)
 })
