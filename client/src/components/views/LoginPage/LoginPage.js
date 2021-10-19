f269dfb853b49f69db9af0723736fc780b9ab0ff
chae <chae@chae.com>
2021-10-20 01:09:10 +0900

    login

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useDispatch} from 'react-redux';
+import { loginUser } from '../../../_actions/user_action';
+//import { request, response } from 'express'
 
-function LoginPage(){
+function LoginPage(props){
+
+    const dispatch = useDispatch();
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+    const onSubmitHandler = (event) => {
+        event.preventDefault(); //refresh를 막기 위해 사용
+
+        let body={
+            email: Email,
+            password: Password
+        }
+
+        dispatch(loginUser(body))
+            .then(response => {
+                if(response.payload.loginSuccess){
+                    props.history.push('/')
+                }else{
+                    alert('Error')
+                }
+            })
+        
+
+    }
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'
+        }}>
+            <form style={{display: 'flex', flexDirection: 'column'}}
+                onSubmit={onSubmitHandler}
+            >
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler} />
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler} />
+                <br />
+                <button type="submit">
+                    Login
+                </button>
+            </form> 
         </div>
     )
 }
